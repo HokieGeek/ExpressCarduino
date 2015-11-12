51bfc585d39d59e0a5caad13ccbae70d6da40f6e
HokieGeek <andres.f.perez@gmail.com>
2015-11-12 00:18:18 -0500

    Greatly refactored the samples I grabbed this from to suit my needs. Tested with Uno

--- ExpressCarduino.ino
+++ ExpressCarduino.ino
@@ -1,100 +1,83 @@
-/*
-  Serial Call and Response
- Language: Wiring/Arduino
-
- This program sends an ASCII A (byte of value 65) on startup
- and repeats that until it gets some data in.
- Then it waits for a byte in the serial port, and
- sends three sensor values whenever it gets a byte in.
-
- Thanks to Greg Shakar and Scott Fitzgerald for the improvements
-
-   The circuit:
- * potentiometers attached to analog inputs 0 and 1
- * pushbutton attached to digital I/O 2
-
- Created 26 Sept. 2005
- by Tom Igoe
- modified 24 April 2012
- by Tom Igoe and Scott Fitzgerald
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/SerialCallResponse
-
- */
-
-int firstSensor = 0;    // first analog sensor
-int secondSensor = 0;   // second analog sensor
-int thirdSensor = 0;    // digital sensor
-int inByte = 0;         // incoming serial byte
+#define buttonPin 2
+#define potPin A0
+#define connectedLedPin 13
 
 String inputString = "";
-boolean stringComplete = false;
+int potValue = -1;
+int targetPotValue = -1;
 
-void setup()
-{
-  // start serial port at 9600 bps:
-  Serial.begin(9600);
+void setup() {
+    Serial.begin(9600);
 
-  pinMode(2, INPUT);   // digital sensor is on digital pin 2
-  establishContact();  // send a byte to establish contact until receiver responds
-  
-  inputString.reserve(200); // Reserve 200 bytes
+    waitForHandshake();  // send a byte to establish contact until receiver responds
+    Serial.setTimeout(3000); // TODO: this is only for the human typing this stuff
+
+    inputString.reserve(200);
+    potValue = analogRead(potPin); // Initialize the pot tracker
+
+    pinMode(buttonPin, INPUT);
 }
 
-void loop()
-{
-  serialEvent();
-  
-  if (stringComplete) {
-    Serial.println(inputString);
-    inputString = "";
-    stringComplete = false;
-  }
-  
-  // if we get a valid byte, read analog ins:
-  if (Serial.available() > 0) {
-    // get incoming byte:
-    inByte = Serial.read();
-    // read first analog input, divide by 4 to make the range 0-255:
-    firstSensor = analogRead(A0) / 4;
-    // delay 10ms to let the ADC recover:
-    delay(10);
-    // read second analog input, divide by 4 to make the range 0-255:
-    secondSensor = analogRead(1) / 4;
-    // read  switch, map it to 0 or 255L
-    thirdSensor = map(digitalRead(2), 0, 1, 0, 255);
-    // send sensor values:
-    Serial.write(firstSensor);
-    Serial.write(secondSensor);
-    Serial.write(thirdSensor);
-  }
+void loop() {
+    // Poll the sensors and inputs
+    pollPotentiometer();
+    pollButton();
+
+    // Send out status?
+    // TODO: sendStatus();
 }
 
-void establishContact() {
-  while (Serial.available() <= 0) {
-    Serial.print('A');   // send a capital A
-    delay(300);
-  }
-}
-
-/*
-SerialEvent occurs whenever a new data comes in the
- hardware serial RX.  This routine is run between each
- time loop() runs, so using delay inside loop can delay
- response.  Multiple bytes of data may be available.
-*/
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag
-    // so the main loop can do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
+void waitForHandshake() {
+    while (Serial.available() <= 0 || Serial.read() != ',') {
+      Serial.print('.');
+      delay(300);
     }
-  }
+    Serial.println("hello");
+    digitalWrite(connectedLedPin, HIGH);
+}
+
+void serialEvent() {
+    int input = 0;
+    while (Serial.available()) {
+        input = Serial.read();
+        switch (input) {
+        case '$':
+            targetPotValue = Serial.parseInt();
+            Serial.print("#");
+            Serial.println(targetPotValue);
+            break;
+        default:
+            char inChar = (char)input;
+            if (inChar == '\n' || inChar == '\r') {
+                Serial.println(inputString);
+                inputString = "";
+            } else {
+                inputString += inChar;
+            }
+            break;
+        }
+    }
+}
+
+void pollPotentiometer() {
+    int currentPotValue = analogRead(potPin);
+    if (currentPotValue != potValue && abs(currentPotValue - potValue) > 1) {
+        potValue = currentPotValue;
+
+        if (targetPotValue >= 0) {
+            if (potValue == targetPotValue) {
+                Serial.print('!');
+            } else if (potValue < targetPotValue) {
+                Serial.print('+');
+            } else {
+                Serial.print('-');
+            }
+        }
+    }
+}
+
+void pollButton() {
+    if (digitalRead(buttonPin) == HIGH) {
+      Serial.println("Press!!");
+    }
 }
