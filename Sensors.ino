8064a4510c3bee4b511dc6b25890301d10f47c54
HokieGeek <andres.f.perez@gmail.com>
2015-11-25 10:10:09 -0500

    Removed the potentiometer stuff since that was just for playing around

--- Sensors.ino
+++ Sensors.ino
@@ -1,30 +1,23 @@
 #define buttonPin 2
-#define potPin A0
 
 #define photocellCount 2
 #define photocell1Pin A1
 #define photocell2Pin A2
 
-int potValue = -1;
-int targetPotValue = -1;
 String inputString = "";
 
 int photocellValues[3] = {0,0,0};
 
 void sensorsSetup() {
-    potValue = analogRead(potPin); // Initialize the pot tracker
     pinMode(buttonPin, INPUT);
 
     inputString.reserve(200);
 }
 
 void sensorsLoop() {
-    pollPotentiometer();
-
     pollAmbientLight();
 
     if (haveHandshake) {
-        writePotentiometer();
         writeButton();
 
         writeAmbientLight();
@@ -33,11 +26,6 @@ void sensorsLoop() {
 
 void sensorsSerialEvent(int input) {
     switch (input) {
-    case '$':
-        targetPotValue = Serial.parseInt();
-        Serial.print("#");
-        Serial.println(targetPotValue);
-        break;
     default:
         char inChar = (char)input;
         if (inChar == '\n' || inChar == '\r') {
@@ -47,27 +35,6 @@ void sensorsSerialEvent(int input) {
             inputString += inChar;
         }
         break;
-    }
-}
-
-void pollPotentiometer() {
-    int currentPotValue = analogRead(potPin);
-    if (currentPotValue != potValue && abs(currentPotValue - potValue) > 1) {
-        potValue = currentPotValue;
-    }
-}
-
-void writePotentiometer() {
-    if (haveHandshake) {
-        if (targetPotValue >= 0) {
-            if (potValue == targetPotValue) {
-                Serial.print('!');
-            } else if (potValue < targetPotValue) {
-                Serial.print('+');
-            } else {
-                Serial.print('-');
-            }
-        }
     }
 }
 
