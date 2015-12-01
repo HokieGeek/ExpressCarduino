abadb0077bfa4416ea9862ff3ff7606b087d3a7d
HokieGeek <andres.f.perez@gmail.com>
2015-12-01 00:58:59 -0500

    Hopefully correctly added a relatively precise tmp36 reading

--- Sensors.ino
+++ Sensors.ino
@@ -1,11 +1,15 @@
+#define AREF_VOLTAGE 3.3
+
 #define buttonPin 2
 
 #define photocellCount 2
 #define photocell1Pin A1
 #define photocell2Pin A2
+#define tempPin A3
 
 boolean sendButtonState = false;
 boolean sendAmbientLight = false;
+boolean sendTemperature = false;
 String inputString = "";
 
 int photocellValues[photocellCount+1] = {0,0,0};
@@ -14,6 +18,8 @@ void sensorsSetup() {
     pinMode(buttonPin, INPUT);
 
     inputString.reserve(200);
+
+    analogReference(EXTERNAL);
 }
 
 void sensorsLoop() {
@@ -25,7 +31,11 @@ void sensorsLoop() {
         }
 
         if (sendAmbientLight) {
-            writeAmbientLight();
+            writeAmbientLightASCII();
+        }
+
+        if (sendTemperature) {
+            writeTemperatureASCII();
         }
     }
 }
@@ -40,15 +50,21 @@ void sensorsSerialEvent(int input) {
         sendAmbientLight = true;
         Serial.write(ACK_CHAR);
         break;
+    case 'T': 
+        sendTemperature = true;
+        Serial.write(ACK_CHAR);
+        break;
     case '*':
-        sendAmbientLight = true;
         sendButtonState = true;
+        sendAmbientLight = true;
+        sendTemperature = true;
         Serial.write(ACK_CHAR);
         break;
     default: // TODO: this is mostly useless, for now.
         char inChar = (char)input;
         if (inChar == '\n' || inChar == '\r') {
             Serial.println(inputString);
+            Serial.write(ACK_CHAR);
             inputString = "";
         } else {
             inputString += inChar;
@@ -89,4 +105,23 @@ void writeAmbientLight() {
     Serial.write("L");
     Serial.write((uint8_t)(photocellCount+1));
     Serial.write((uint8_t*)photocellValues, sizeof(photocellValues));
+}
+
+int getTemperature() {
+    int temp = analogRead(tempPin);
+
+    float voltage = temp * AREF_VOLTAGE;
+    voltage /= 1024.0;
+
+    float tempC = (voltage - 0.5) * 100;
+
+    float tempF = (tempC * 9.0 / 5.0) + 32.0;
+
+    return tempF;
+}
+
+void writeTemperatureASCII() {
+    Serial.print("T");
+    Serial.print(getTemperature());
+    Serial.println("");
 }
